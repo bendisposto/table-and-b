7317837eeb6f46a124c7b50a96d60e4e56b32668
Jens Bendisposto <jens@bendisposto.de>
2015-12-13 12:08:55 +0100

    aufräumen / rewrite

--- src/table_to_b/core.clj
+++ src/table_to_b/core.clj
@@ -1,216 +1,88 @@
 (ns table-to-b.core
   (:require [dk.ative.docjure.spreadsheet :as dj]
-            [clojure.java.io :as io]
             [stencil.core :as sc]
             [clojure.string :as st])
-  (:import de.be4.classicalb.core.parser.BParser
-           de.be4.classicalb.core.parser.exceptions.BException)
   (:gen-class))
 
-(comment 
-  (def workbook
-  "läd spreadsheet.xlsx"
-  (dj/load-workbook "spreadsheet.xlsx"))
+(defmacro forv [& body]
+  `(into [] (for ~@body)))
 
-  (def s
-  "tabelle Price List"
-  (dj/select-sheet "Price List" workbook)))
+(defn tuple-encoding [name table-name data]
+  (sc/render-string
+   "
+MACHINE {{machine}}
+ DEFINITIONS TUPLETYPE == ({{tuple-type}})
+ ABSTRACT_CONSTANTS {{accessor-names}}
+ CONSTANTS {{table-name}}
 
-(def datatype
-  "Zuordnung Datentypnr -> Bezeichnung"
-  {:0 "INTEGER" :1 "STRING" :2 "Formula" :3 "Blank" :4 "BOOL" :5 "Error"})
+ PROPERTIES
+  {{table-name}} : POW(TUPLETYPE) & 
+  {{table-name}} = { 
+   {{{tuples}}} 
+  } &
+  {{{accessor-functions}}}
+END
+" (assoc data :machine name :table-name table-name)))
 
-(defn get-datatype
-  "ordnet der Typnummer den Datentyp zu"
-  [typenr]
-  (get datatype ((comp keyword str) typenr)))
+(def types ["INTEGER" "STRING" :formula :blank "BOOL" :error])
 
-(defn get-type
-  "Gibt den Typ der übergeben Zelle als Zahl zurück, Zuordnung siehe 'datatype'"
-  [cell]
-  (when cell
-    (.getCellType cell)))
+(defn row-width [row] (count (dj/cell-seq row)))
 
-(defn element
-  "Gibt das übergebene Element mit Anführungszeichen zurück, wenn es vom Typ String ist
-  sonst ohne"
-  [el]
-  (let [typ (get-type el)]
-    	(if (= typ 1)
-        (str \" el \")
-        (if (= typ 0)
-          (int (dj/read-cell el))
-          (str el)))))
+(defn extract-table [cols rows]
+  (forv [r rows]
+        (forv [col (range cols)]
+              (do
+                (let [cell (.getCell r col)]
+                  (when cell
+                    (let [value (dj/read-cell cell)]
+                      {:value (if (float? value) (int value) value)
+                       :type (types (.getCellType cell))
+                       :row (.getRowIndex cell)
+                       :col (.getColumnIndex cell)})))))))
 
-(defn wert
-  "Gibt den Wert der angegebenen Zelle wieder und wenn sie leer ist, \"\""
-  [row i]
-  (if (nil? (.getCell row i))
-    "\"\""
-    (element (.getCell row i))))
+(defn fix-types [column]
+  (let [etype (:type (first column))
+        problems (remove #(= etype (:type %)) column)]
+    (doseq [p problems]
+      (binding [*out* *err*]
+        (println "WARNING" (sc/render-string "Type mismatch in row {{row}} column {{col}}. Expected type is {{etype}} but value is of type {{type}}" (assoc p :etype etype)))))
+    (if (seq problems)
+      (map #(assoc % :type "STRING" :value (str (:value %))) column)
+      column)))
 
-(defn row-as-tuple
-  "Gibt die Daten einer Reihe als Tuple zurück
-  angepasst nach Typ mit Anführungszeichen oder ohne"
-  [row anz]
-  (loop [i 0 reihe row erg []]
-    (if (<= anz i)
-      erg
-      (recur (inc i) (rest reihe) (conj erg (wert row i))))))
+(defn ascii [c]
+  (let [i (int c)]
+    (or (< 64 i 91) (< 96 i 123))))
 
-(defn get-cell
-  "Gibt den Inhalt der n. Zelle Zeile row wieder"
-  [n row]
-  (.getCell row n))
+(defn make-tuple [row]
+  (let [elements (map (comp pr-str :value) row)]
+    (str "(" (st/join "," elements) ")")))
 
-(defn types-of-nth-column
-  "Gibt die Typen der Zellen der n. Spalte der Tabelle sheet als Zahlen wieder"
-  [n rows]
-  (map (comp get-type (partial get-cell n)) rows))
+(defn make-accessor [n id name]
+  (let [vars (st/join "," (for [x (range n)] (str "v" x)))]
+    (sc/render-string
+     "{{name}} = %({{vars}}).(({{vars}}):TUPLETYPE|v{{id}})"
+     {:name name :vars vars :id id})))
 
-(defn haupttyp
-  "Gibt den Namen der typen wieder, wenn er bei allen gleich ist,
-  sonst 'STRING'"
-  [typen]
-  (if (apply = typen)
-    (get-datatype (first typen))
-    "STRING"))
-
-(defn haupttypen
-  "Gibt die Datentypen der Reihen der Tabelle als list wieder"
-  [titles data]
-  (loop [i (count titles) erg []]
-    (if (> 0 i )
-      (reverse (rest erg))
-      (recur (dec i) (conj erg (haupttyp (types-of-nth-column i data)))))))
-
-(defn valid-b-identifier?
-  "prüft, ob es sich bei dem übergebenen string um einen validen B-Identifier handelt"
-  [string]
-  (try
-    (do  (BParser/parse (str "#FORMULA " string))
-         true)
-    (catch BException e false)))
-
-(defn pruefe_titel
-  "überprüft, ob es sich bei dem Titel um einen validen B-Identifier handelt,
-  wenn nein, ersetzt alle Leerzeichen durch _ und prüft erneut
-  wenn der Fehler immer noch nicht behoben wurde, wird das Programm geschlossen"
-  [titel]
-  (if (valid-b-identifier? titel)
-    titel
-    (if (valid-b-identifier? (st/replace titel " " "_"))
-      (st/replace titel " " "_")
-      ((println titel " ist kein gültiger B-Identifier, deshalb wird das Programm beendet.")
-      (System/exit 0)))))
-
-(defn ermittel_titel
-  "prüft für alle Elemente des Vektors, ob es sich um einen gültigen Titel handelt
-  und passt ihn ggf an"
-  [titles]
-  (map pruefe_titel titles))
-
-(defn tupletype
-  "Gehört zur tupleversion,
-  Ermittelt die Werte für den tupletype-tag"
-  [titles data]
-  (st/join "*" (haupttypen titles data)))
-
-(defn tuples
-  "Gehört zur tupleversion,
-  Ermittelt die Werte für den tuple-tag im tuples-tag"
-  [rows n]
-  (into []
-    (for [row rows]
-      {:tuple (st/join ", " (row-as-tuple row n))})))
-
-(defn tuplefunctions
-  ""
-  [titles rows]
-  (loop [i 0 erg []]
-    (if (= (count titles) i)
-      erg
-      (recur (inc i) (conj erg {:title (nth titles i) :id i})))))
-
-(defn tuplemachine
-  "Erstellt die Tuplemachine mit Hilfe des templates"
-  [sheet]
-  (let [rows (dj/row-seq sheet)
-        erste_Reihe (map dj/read-cell (first rows))
-        titles (ermittel_titel erste_Reihe)
-        data (rest rows)
-        zuordnung (assoc {}
-          :tupletype (tupletype titles data)
-          :titles (st/join ", " titles)
-          :tuples (tuples data (count titles))
-          :functions (tuplefunctions titles data)
-          :vars (st/join ",x" (range 0 (count titles))))]
-    zuordnung))
-
-(defn zuordnung
-  "Erstellt eine Zuordnung vom Muster name1:wert1,name2:wert2..."
-  [namen werte]
-  (subs (reduce str (interleave (repeat ",") namen (repeat ":") werte )) 1))
-
-(defn rec
-  "Gehört zur recordversion,
-  Ermittelt die Werte für den titles_Werte-tag im rec-tag"
-  [rows titles]
-  (into []
-    (for [row rows]
-      { :titles_Werte (zuordnung titles (row-as-tuple row (count titles))) } )))
-
-(defn functions
-  "Gehört zur recordversion,
-  Ermittelt die Werte für den title-tag und den id-tag im functions-tag"
-  [titles]
-  (into []
-    (for [title titles]
-      (let [x (gensym "x")] {:title title :id x}))))
-
-(defn titles_haupttypen
-  "Gehört zur recordversion,
-  Ermittelt die Werte für den titles_haupttypen-tag"
-  [titles data]
-  (zuordnung titles (haupttypen titles data)))
-
-(defn recordmachine
-  "erstellt die Recordmachine mit Hilfe des templates"
-  [sheet]
-  (let [rows (dj/row-seq sheet)
-        erste_Reihe (map dj/read-cell (first rows))
-        titles (ermittel_titel erste_Reihe)
-        data (rest rows)
-        zuordnung (assoc {}
-          :titles_haupttypen (titles_haupttypen titles data)
-          :titles (st/join ", " titles)
-          :rec (rec data titles)
-          :functions (functions titles))]
-    zuordnung))
-
-(defn render-to-file! [zuordnung template filename]
-  (spit filename
-        (sc/render-file (str "templates/" template) zuordnung)))
-
-(defmulti foo (fn [a b _] [a (count b)]))
-(defmethod foo [:test 3] [a b c] (println a))
-(defmethod foo [:test 4] [a b c](println b))
-(defmethod foo [:bar 3] [a b c](println c))
-
-(def usage "java -jar foobar.jar [record|tuple] Excelfile Sheet-Name Target-B-Filename")
-
-(defmulti create (fn [version _] (keyword version)))
-(defmethod create :tuple [_ sheet] (tuplemachine sheet))
-(defmethod create :record [_ sheet] (recordmachine sheet))
-(defmethod create :default [_ sheet] (println usage))
-
+(defn parse-excel [filename sheetname]
+  (let [workbook (dj/load-workbook filename)
+        sheet (dj/select-sheet sheetname workbook)
+        rows (dj/row-seq sheet)
+        header (first rows)
+        table (extract-table (count (dj/cell-seq header)) (rest rows))
+        transposed (mapv fix-types (apply map vector table))
+        table (apply map vector transposed)
+        types (map (comp :type first) transposed)
+        header (forv [c (dj/cell-seq header)] (apply str (filter ascii (dj/read-cell c))))]
+    {:header header
+     :table table
+     :transposed transposed
+     :types types
+     :accessor-names (st/join "," header)
+     :accessor-functions (st/join " &\n  " (map-indexed (partial make-accessor (count header)) header))
+     :tuple-type (st/join "*" types)
+     :tuples (st/join ",\n   " (map make-tuple table))}))
 
 (defn -main
-  "ruft, je nach Parametern, die Methode zu tuple oder record erstellen auf"
-  [version dateiname s1 machinefile]
-  (let [wb (dj/load-workbook dateiname)
-        sheet (dj/select-sheet s1 wb)]
-    (cond
-      (= version "record") (-> sheet recordmachine (render-to-file! "recordversion" machinefile))
-      (= version "tuple") (-> sheet tuplemachine (render-to-file! "tupleversion" machinefile))
-      :otherwise (println usage))))
+  [xls-file sheet table-name]
+  (println (tuple-encoding (apply str (filter ascii sheet)) table-name (parse-excel xls-file sheet))))
